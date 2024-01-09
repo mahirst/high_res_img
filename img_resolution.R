576bc35bded1f9d0eab77f2f7fc9a126216373dd
Molly Hirst <108477568+mahirst@users.noreply.github.com>
2024-01-09 15:06:38 -0500

    Add files via upload

---
+++ img_resolution.R
@@ -0,0 +1,16 @@
+# make sure you are in the same working directory as your image
+
+jpeg(
+  filename="Rplot.jpeg",
+  width=8,
+  height=8,
+  units="in",
+  res=1000)
+  
+
+
+# insert code used to make image here
+
+
+
+dev.off()
